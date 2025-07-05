91dea9bae2ceaefb95a7877796d1a1a393b39ab2
iantasena <puantorian12@gmail.com>
2025-07-05 15:55:53 +0700

    update sedikit

--- My_CV/src/components/Skills.vue
+++ My_CV/src/components/Skills.vue
@@ -3,7 +3,7 @@ import SectionTitle from './SectionTitle.vue';
 const skills = [
 { name: 'Vue.js', level: 'Mahir' }, { name: 'JavaScript', level:
 'Mahir' },
-{ name: 'Tailwind CSS', level: 'Mahir' }, { name: 'Node.js', level:
+{ name: 'Tailwind CSS', level: 'Mahir' }, { name: 'Bootsrap CSS', level: 'Menengah' }, { name: 'Node.js', level:
 'Menengah' },{ name: 'Express.js', level: 'Menengah' }, { name: 'PostgreSQL',
 level: 'Menengah' },
 { name: 'Git & GitHub', level: 'Mahir' }, { name: 'HTML5 & CSS3',
