1c628e8384f6d47c3ab42fc95ab2a0d28a851d68
iantasena <puantorian12@gmail.com>
2025-07-09 11:06:09 +0700

    fix minor error

--- My_CV/src/main.js
+++ My_CV/src/main.js
@@ -3,7 +3,7 @@ import './assets/main.css';
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
-import animateOnScroll from './directives/animateOnScroll'; // <-- Impor
+import animateOnScroll from './Directives/animateOnScroll'; // <-- Impor
 const app = createApp(App);
 app.directive('animate-on-scroll', animateOnScroll); // <-- Daftarkan
 app.use(router);
