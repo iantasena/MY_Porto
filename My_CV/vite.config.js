69a2dbe5a1b9ab167470ff98aedc2e8b09cdd80f
iantasena <puantorian12@gmail.com>
2025-07-11 11:05:38 +0700

    Update

--- My_CV/vite.config.js
+++ My_CV/vite.config.js
@@ -9,9 +9,7 @@ export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
-
   ],
-  base: './',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
