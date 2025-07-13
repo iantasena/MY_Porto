45ef0ce203b68e2a1ea93f58042a92d76b05973d
iantasena <puantorian12@gmail.com>
2025-07-13 22:13:18 +0700

    up

--- My_CV/src/components/Skills.vue
+++ My_CV/src/components/Skills.vue
@@ -23,16 +23,19 @@ onMounted(async () => {
     <div class="container mx-auto px-6">
       <SectionTitle title="Keahlian & Teknologi" />
       <div class="grid grid-cols-2 md:grid-cols-4 gap-6 mt-12">
-        <div v-for="skill in skills" :key="skill.name" class="bg-sw-panel p-4 rounded-lg border border-sw-border text-left">
-  <div class="flex justify-between items-center mb-1">
-    <h3 class="font-sans text-sw-dark font-bold">{{ skill.name }}</h3>
-    <p class="text-sm text-sw-text">{{ skill.level }}</p>
+       <div class="grid grid-cols-2 md:grid-cols-4 gap-6 mt-12">
+
+  <div v-for="skill in skills" :key="skill.name" class="bg-sw-panel p-4 rounded-lg border border-sw-border text-left">
+    <div class="flex justify-between items-center mb-1">
+      <h3 class="font-sans text-sw-dark font-bold">{{ skill.name }}</h3>
+      <p class="text-sm text-sw-text">{{ skill.level }}</p>
+    </div>
+    <div class="w-full bg-sw-border rounded-full h-2.5">
+      <div class="bg-sw-accent h-2.5 rounded-full" :style="{ width: skill.width }"></div>
+    </div>
   </div>
-  <div class="w-full bg-sw-border rounded-full h-2.5">
-    <div class="bg-sw-blue h-2.5 rounded-full shadow-lg shadow-sw-blue/50" :style="{ width: skill.width }"></div>
   </div>
-</div>
-      </div>
+    </div>
     </div>
   </section>
 </template>
