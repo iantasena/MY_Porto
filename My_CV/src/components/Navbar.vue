620035b4d80d704d342be96a531bf876d606afaf
iantasena <puantorian12@gmail.com>
2025-07-14 00:04:53 +0700

    up

--- My_CV/src/components/Navbar.vue
+++ My_CV/src/components/Navbar.vue
@@ -67,17 +67,5 @@ const closeMenu = () => {
 
 <style scoped>
 /* 5. Tambahkan CSS untuk animasi <Transition> */
-.slide-fade-enter-active {
-  transition: all 0.3s ease-out;
-}
 
-.slide-fade-leave-active {
-  transition: all 0.2s cubic-bezier(1, 0.5, 0.8, 1);
-}
-
-.slide-fade-enter-from,
-.slide-fade-leave-to {
-  transform: translateY(-20px);
-  opacity: 0;
-}
 </style>
